046f5a96ea3eaf42769dfabce54175d828713083
senthan-07 <senthanbalu@gmail.com>
2025-05-19 18:36:05 +0530

    fixed some issues

--- apps/backend/src/controllers/AuthHandler.ts
+++ apps/backend/src/controllers/AuthHandler.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import  jwt  from "jsonwebtoken";
 import { SignupRequest ,SigninRequest } from "../types/auth";
 
 dotenv.config();
@@ -53,7 +54,17 @@ export const signinHandler = async (req: SigninRequest, res: any) => {
       return res.status(400).json({ error: "Invalid credentials" });
     }
 
-    res.json({ message: "MFA code sent to your email" });
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET!,  // Make sure to store your secret key in .env
+      { expiresIn: '5h' }       // Set expiration to 1 hour (adjustable)
+    );
+
+    // Send the token to the user
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error logging in" });
