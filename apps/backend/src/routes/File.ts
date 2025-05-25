984de693af48fcef65500fd7ee89f4758b2a21fd
senthan-07 <senthanbalu@gmail.com>
2025-05-25 19:00:40 +0530

    File handling finished

--- apps/backend/src/routes/File.ts
+++ apps/backend/src/routes/File.ts
@@ -1,10 +1,11 @@
 import  express, { Router }  from "express";
 import { deleteHandler, getHandler, sendHandler } from "../controllers/FileHandler";
 import authenticateToken from "../middlewares/Authmiddleware";
+import { upload } from "../middlewares/multer";
 const fileRouter = Router()
 
-fileRouter.get("/:id",authenticateToken,getHandler);
-fileRouter.post("/:id",authenticateToken,sendHandler);
-fileRouter.delete("/delete/:id",authenticateToken,deleteHandler);
+fileRouter.get("/:title",authenticateToken,getHandler);
+fileRouter.post("/",upload.single("file"),authenticateToken,sendHandler);
+fileRouter.delete("/:title/:id",authenticateToken,deleteHandler);
 
 export {fileRouter}
