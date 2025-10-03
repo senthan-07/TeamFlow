6ff44b58f2b5a28b3bee1164a7cde574c1c7020a
senthan-07 <senthanbalu@gmail.com>
2025-10-03 11:24:12 +0530

    new branch for testing

--- apps/frontend/lib/axios.ts
+++ apps/frontend/lib/axios.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
-import { useAuthStore } from "@/store/authStore";
+import { useAuthStore } from "../store/authStore";
 
 //Basically middleware for adding bearer token
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
 });
-
+// console.log(api);
 //intercerptor is like burp intercept req check if its in browser then add bearer token
 api.interceptors.request.use((config) => {
-  // const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-  const token = useAuthStore.getState().token;
+  // const tokenFromStore = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const tokenFromStorage = useAuthStore.getState().token;
+  const token =  tokenFromStorage;
+  // console.log(token)
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
